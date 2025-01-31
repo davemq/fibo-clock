84d724b3ffffdbdb6d291537e4446b06e957516c
Dave Marquardt <davemarq@linux.ibm.com>
2025-01-31 14:47:19 -0600

    Implemented Fibonacci clock up to number 34. This allows minutes and seconds to be represented directly.

--- fiboclock.tcl
+++ fiboclock.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/wish
 
 # globals
-set secincrement 60
-set hmdivisor    2
+set secincrement 1
+set hmdivisor    1
 set minincrement [expr $hmdivisor * 60]
 
 set seconds_mode 0;		# really sub-minute units
@@ -59,7 +59,7 @@ proc update_colors {} {
 	set s [expr $s / $secincrement]
     }
 
-    foreach l [list {13 .c13} {8 .c8} {5 .c5} {3 .c3} {2 .c2} {1 .c1} {1 .c12}] {
+    foreach l [list {34 .c34} {21 .c21} {13 .c13} {8 .c8} {5 .c5} {3 .c3} {2 .c2} {1 .c1} {1 .c12}] {
 
 	set i [lindex $l 0]
 	set w [lindex $l 1]
@@ -90,6 +90,8 @@ proc update_colors {} {
 
 
 # Set up canvas widgets
+canvas .c34 -height 0 -width 0 -background white -relief solid -bd 1
+canvas .c21 -height 0 -width 0 -background white -relief solid -bd 1
 canvas .c13 -height 0 -width 0 -background white -relief solid -bd 1
 canvas .c8 -height 0 -width 0 -background white -relief solid -bd 1
 canvas .c5 -height 0 -width 0 -background white -relief solid -bd 1
@@ -99,24 +101,23 @@ canvas .c1 -height 0 -width 0 -background white -relief solid -bd 1
 canvas .c12 -height 0 -width 0 -background white -relief solid -bd 1
 
 # Place canvas widgets in grid
-# grid .c2 .c1  .c5 -sticky nsew
-# grid ^   .c12 ^   -sticky nsew
-# grid .c3 -    ^    -sticky nsew
-
-grid .c5 .c3  -   .c13 -sticky nsew
-grid ^   .c12 .c2 ^    -sticky nsew
-grid ^   .c1  ^   ^    -sticky nsew
-grid .c8 -    -   ^    -sticky nsew
+grid .c13 .c8 -    -   .c34 -sticky nsew
+grid ^    .c2 .c1  .c5 ^    -sticky nsew
+grid ^    ^   .c12 ^   ^    -sticky nsew
+grid ^    .c3 -    ^   ^    -sticky nsew
+grid .c21 -   -    -   ^    -sticky nsew
 
 # Configure grid weights
-grid rowconfigure . 0 -weight 3
+grid rowconfigure . 0 -weight 8
 grid rowconfigure . 1 -weight 1
 grid rowconfigure . 2 -weight 1
-grid rowconfigure . 3 -weight 8
-grid columnconfigure . 0 -weight 5
-grid columnconfigure . 1 -weight 1
-grid columnconfigure . 2 -weight 2
-grid columnconfigure . 3 -weight 13
+grid rowconfigure . 3 -weight 3
+grid rowconfigure . 4 -weight 21
+grid columnconfigure . 0 -weight 13
+grid columnconfigure . 1 -weight 2
+grid columnconfigure . 2 -weight 1
+grid columnconfigure . 3 -weight 5
+grid columnconfigure . 4 -weight 34
 
 # Key bindings
 bind . q exit
